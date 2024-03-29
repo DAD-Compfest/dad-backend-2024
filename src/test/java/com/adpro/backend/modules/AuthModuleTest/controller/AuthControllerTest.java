ec993700b05990ce71aad1b30954986352c3eb96
isaui <isadestroyed17@gmail.com>
2024-03-29 12:35:43 +0700

    clean: remove unused dependency

--- src/test/java/com/adpro/backend/modules/AuthModuleTest/controller/AuthControllerTest.java
+++ src/test/java/com/adpro/backend/modules/AuthModuleTest/controller/AuthControllerTest.java
@@ -8,12 +8,10 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
