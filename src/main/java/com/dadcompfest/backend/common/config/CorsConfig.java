75303b0b6b6569c2e96d0740513c62f677fddcea
riozulfandy <rio.zulfandy07@gmail.com>
2024-04-10 02:31:50 +0800

    feat: samesite attribute to cookies and try reconfig cors

---
+++ src/main/java/com/dadcompfest/backend/common/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.dadcompfest.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true);
+    }
+}
